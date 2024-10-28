9437ad674622569f02a20622bad1f43176b4e4c1
Alvin <alvinhuang9@gmail.com>
2024-10-28 15:52:49 +0000

    addedall other graphs and code for algorithm

--- self_fusion.py
+++ self_fusion.py
@@ -1,10 +1,11 @@
 import imufusion
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 # Import sensor data ("short_walk.csv" or "long_walk.csv")
-data = numpy.genfromtxt("data_files/forward_back.csv", delimiter=",", skip_header=1)
+data = np.genfromtxt("data_files/forward_back.csv", delimiter=",", skip_header=1)
 
+# sample rate of our sensor
 sample_rate = 50 
 
 timestamp = data[:, 0]
@@ -12,7 +13,7 @@ gyroscope = data[:, 1:4]
 accelerometer = data[:, 4:7]
 magnetometer = data[:, 7:10]
 
-figure, axes = plt.subplots(nrows=3, sharex=True)
+figure, axes = plt.subplots(nrows=5, sharex=True)
 
 axes[0].plot(timestamp, gyroscope[:, 0], "tab:red", label="Gyroscope X")
 axes[0].plot(timestamp, gyroscope[:, 1], "tab:green", label="Gyroscope Y")
@@ -38,6 +39,121 @@ axes[2].set_title("Magnetometer Readings")
 axes[2].grid()
 axes[2].legend()
 
-axes[2].set_xlabel("time/s")
+axes[4].set_xlabel("time/s")
+
+# initialise the AHRS algo
+offset = imufusion.Offset(sample_rate)
+ahrs = imufusion.Ahrs()
+
+ahrs.settings = imufusion.Settings(imufusion.CONVENTION_NWU,
+                                   0.5,  # gain, chooses between gyro or mag+accel
+                                   250,  # gyroscope range 250 for icm
+                                   10,  # acceleration rejection
+                                   10,  # magnetic rejection
+                                   5 * sample_rate)  # rejection timeout = 5 seconds
+
+# Process sensor data
+delta_time = np.diff(timestamp, prepend=timestamp[0])
+
+euler = np.empty((len(timestamp), 3))
+internal_states = np.empty((len(timestamp), 6))
+flags = np.empty((len(timestamp), 4))
+acceleration = np.empty((len(timestamp), 3))
+gravity = np.empty((len(timestamp), 3))
+
+for index in range(len(timestamp)):
+    gyroscope[index] = offset.update(gyroscope[index])
+
+    ahrs.update(gyroscope[index], accelerometer[index], magnetometer[index], delta_time[index])
+
+    euler[index] = ahrs.quaternion.to_euler()
+
+    ahrs_internal_states = ahrs.internal_states
+    internal_states[index] = np.array([ahrs_internal_states.acceleration_error,
+                                       ahrs_internal_states.accelerometer_ignored,
+                                       ahrs_internal_states.acceleration_recovery_trigger,
+                                       ahrs_internal_states.magnetic_error,
+                                       ahrs_internal_states.magnetometer_ignored,
+                                       ahrs_internal_states.magnetic_recovery_trigger,])
+
+    acceleration[index] = 9.81 * ahrs.earth_acceleration  # convert g to m/s/s
+    gravity[index] = 9.81 * ahrs.gravity
+
+    ahrs_flags = ahrs.flags
+    flags[index] = np.array(
+        [   ahrs_flags.initialising,
+            ahrs_flags.angular_rate_recovery,
+            ahrs_flags.acceleration_recovery,
+            ahrs_flags.magnetic_recovery,
+        ])
+    
+axes[3].plot(timestamp, gravity[:, 0], "tab:red", label="Gravity X")
+axes[3].plot(timestamp, gravity[:, 1], "tab:green", label="Gravity Y")
+axes[3].plot(timestamp, gravity[:, 2], "tab:blue", label="Gravity Z")
+axes[3].set_ylabel("g")
+axes[3].set_title("Gravity Readings")
+axes[3].grid()
+axes[3].legend()
+
+axes[4].plot(timestamp, acceleration[:, 0], "tab:red", label="Acceleration X")
+axes[4].plot(timestamp, acceleration[:, 1], "tab:green", label="Acceleration Y")
+axes[4].plot(timestamp, acceleration[:, 2], "tab:blue", label="Acceleration Z")
+axes[4].set_ylabel("g")
+axes[4].set_title("Accelerometer Readings")
+axes[4].grid()
+axes[4].legend()
+    
+def plot_bool(axis, x, y, label):
+    axis.plot(x, y, "tab:cyan", label=label)
+    plt.sca(axis)
+    plt.yticks([0, 1], ["False", "True"])
+    axis.grid()
+    axis.legend()
+
+# Plot Euler angles
+figure2, axes2 = plt.subplots(nrows=11, sharex=True, gridspec_kw={"height_ratios": [6, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1]})
+
+figure2.suptitle("Euler angles, internal states, and flags")
+
+axes2[0].plot(timestamp, euler[:, 0], "tab:red", label="Roll")
+axes2[0].plot(timestamp, euler[:, 1], "tab:green", label="Pitch")
+axes2[0].plot(timestamp, euler[:, 2], "tab:blue", label="Yaw")
+axes2[0].set_ylabel("Degrees")
+axes2[0].grid()
+axes2[0].legend()
+
+# Plot initialising flag
+plot_bool(axes2[1], timestamp, flags[:, 0], "Initialising")
+
+# Plot angular rate recovery flag
+plot_bool(axes2[2], timestamp, flags[:, 1], "Angular rate recovery")
+
+# Plot acceleration rejection internal states and flag
+axes2[3].plot(timestamp, internal_states[:, 0], "tab:olive", label="Acceleration error")
+axes2[3].set_ylabel("Degrees")
+axes2[3].grid()
+axes2[3].legend()
+
+plot_bool(axes2[4], timestamp, internal_states[:, 1], "Accelerometer ignored")
+
+axes2[5].plot(timestamp, internal_states[:, 2], "tab:orange", label="Acceleration recovery trigger")
+axes2[5].grid()
+axes2[5].legend()
+
+plot_bool(axes2[6], timestamp, flags[:, 2], "Acceleration recovery")
+
+# Plot magnetic rejection internal states and flag
+axes2[7].plot(timestamp, internal_states[:, 3], "tab:olive", label="Magnetic error")
+axes2[7].set_ylabel("Degrees")
+axes2[7].grid()
+axes2[7].legend()
+
+plot_bool(axes2[8], timestamp, internal_states[:, 4], "Magnetometer ignored")
+
+axes2[9].plot(timestamp, internal_states[:, 5], "tab:orange", label="Magnetic recovery trigger")
+axes2[9].grid()
+axes2[9].legend()
+
+plot_bool(axes2[10], timestamp, flags[:, 3], "Magnetic recovery")
 
 plt.show()
